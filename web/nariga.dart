af1666c22d9809a6ccdea9888abaae3694b51bd9
segfault <sf@sharped.net>
2014-11-20 01:36:26 +0100

    -added NodeValidatorBuilder -removed angularjs support (controller etc.) -temp hiding expired deadline (work on this SOON) -collapsable courses added then removed (LOOK AT THIS SOON, THERE ARE STILL SOME USELESS CLASSES PENDING REMOVAL!) -added toolbuttons and their management (stub, incomplete) -setPlace passes also title element now -added last 60 seconds diplay to deadlines -fixed with computing based on window actual width (need review, it also updates not in relatime for now) -messed around with sizes -added autocomplete js stub

--- web/nariga.dart
+++ web/nariga.dart
@@ -9,17 +9,26 @@
 import 'dart:html';
 import 'dart:convert';
 import 'dart:async';
-import 'package:avl_tree/avl_tree.dart';
-import 'package:angular/angular.dart';
-import 'package:ng_infinite_scroll/ng_infinite_scroll.dart';
-import 'package:angular/application_factory.dart';
+//import 'package:avl_tree/avl_tree.dart';
+//import 'package:angular/angular.dart';
+//import 'package:ng_infinite_scroll/ng_infinite_scroll.dart';
+//import 'package:angular/application_factory.dart';
 import 'package:crypto/crypto.dart';
+import 'package:bootjack/bootjack.dart';
+import 'package:dquery/dquery.dart';
+import 'session_manager.dart';
 
+final NodeValidatorBuilder _htmlValidator=new NodeValidatorBuilder.common()
+  ..allowElement('a', attributes: ['href', 'data-target', 'data-toggle', 'data-placement', 'data-html'])
+  ..allowElement('button', attributes: ['data-target', 'data-toggle'])
+  ..allowElement('span', attributes: ['aria-hidden']);
+
+/*
 class NarigaModule extends Module {
   NarigaModule() {
     install(new InfiniteScrollModule());
   }
-}
+}*/
 
 // how often update countdowns? (no json requests)
 const int UPDATE_DELAY = 5000; // 30secs
@@ -29,14 +38,14 @@ Map courseMap;
 Map narigaMap;
 List narigaList;
 
-
+/*
 @Controller(selector: "[nariga]", publishAs: "ctrl")
 class TestController {
   var disabled = false;
   void loadMore() {
     //loadData();
   }
-}
+}*/
 
 class Nariga {
 
@@ -55,19 +64,27 @@ int loadedCourses;
 
 void main() {
 
+  Bootjack.useDefault();
+  /*
+  Collapse.use();
+  Dropdown.use();
+  Transition.use();
+  */
+  
   loadingData = true;
   loadedCourses = 0;
   courseMap = new Map<String, bool>();
   narigaList = new List<Nariga>();
   narigaMap = new Map<int, int>();
 
+  /*
   // init angular module
   var module = new Module();
   module.bind(TestController);
   module.install(new InfiniteScrollModule());
 
   // ready to go with angular module
-  applicationFactory().addModule(module).run();
+  applicationFactory().addModule(module).run();*/
 
   // load available courses
   loadCourses();
@@ -113,7 +130,6 @@ void main() {
   scores['Bob'];      // 36
   scores['Rohan'];    //  5
   scores['Sophena'];  //  7*/
-
 }
 
 
@@ -172,6 +188,7 @@ void onDataLoaded(HttpRequest req) {
     int id = deaddata["id"];
 
     // Add nariga object to exst list
+    
     var newNariga = new Nariga(course, assign, deadline, url, id);
     narigaList.add(newNariga);
 
@@ -191,8 +208,11 @@ void onDataLoaded(HttpRequest req) {
 void workNarigas() {
   // functional style rocks
   narigaList.sort((x, y) => x.deadline.compareTo(y.deadline));
+  DateTime now = new DateTime.now();
   for (var nariga in narigaList) {
-    checkDead(nariga.name, nariga.assign, nariga.deadline, nariga.id);
+    if (now.isBefore(new DateTime.fromMillisecondsSinceEpoch(nariga.deadline * 1000, isUtc: true))) {
+      checkDead(nariga.name, nariga.assign, nariga.deadline, nariga.id);
+    }
   }
 }
 
@@ -229,12 +249,52 @@ void checkDead(String course, String assign, int deadline, int id) {
     </div>
  */
 
-void addDeadList(String course, String assign, int deadline, int id) {
+/*
+       <div class="panel panel-default">
+        <div class="panel-heading">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+              <div class="nariga">
+                <div class="lefts left1w">1w</div>
+                <div class="titles">
+                  <div class="coursename">The Data Scientist’s Toolbox</div>
+                  <div class="coursetask">Week 1 Quiz (Hard Deadline)</div>
+                </div>
+              </div>
+            </a>
+        </div>
+        <div id="collapse1" class="panel-collapse collapse">
+          <div class="panel-body">
+          <span class="label label-danger">Danger</span>
+           </div>
+        </div>
+ */
+/* old
+ * void addDeadList(String course, String assign, int deadline, int id) {
 
   // find the dead anchor
   deadList = querySelector('#deadlist');
 
   // news divs are borning
+  var newDivPanel = new DivElement();
+  var newDivPanelHeading = new DivElement();
+  var newDivPanelA = new Element.a();
+  
+  var collapse = 'collapse' + id.toString();
+  newDivPanel.classes.add('panel panel-default');
+  newDivPanelHeading.classes.add('panel-heading');
+  newDivPanelA.setAttribute('data-toggle', 'collapse');
+  newDivPanelA.setAttribute('data-parent', '#accordion');
+  newDivPanelA.setAttribute('href', '#'+collapse);
+  
+  var newDivCollapse = new DivElement();
+  var newDivPanelBody = new DivElement();
+  
+  newDivCollapse.id = collapse;
+  newDivCollapse.classes.add('panel-collapse collapse');
+  newDivPanelBody.classes.add('panel-body');
+  newDivPanelBody.innerHtml = buildNarigaBody(deadline, id);
+  //newDivPanelBody.text = buildNarigaBody(deadline);
+  
   var newDivNariga = new DivElement();
   var newDivLefts = new DivElement();
   var newDivTitles = new DivElement();
@@ -258,15 +318,138 @@ void addDeadList(String course, String assign, int deadline, int id) {
   newDivTitles.append(newDivCoursetask);
   newDivNariga.append(newDivLefts); // choose deads carefully then
   newDivNariga.append(newDivTitles);
+  newDivPanelA.append(newDivNariga);
+  newDivPanelHeading.append(newDivPanelA);
+  
+  newDivCollapse.append(newDivPanelBody);
+  newDivPanel.append(newDivPanelHeading);
+  newDivPanel.append(newDivCollapse);
+
+  // add it
+  deadList.children.add(newDivPanel);
+
+  // fit it
+  setPlace(newDivLefts, newDivTitles, deadline, id);
+  
+
+}
+ */
+void addDeadList(String course, String assign, int deadline, int id) {
+
+  // find the dead anchor
+  deadList = querySelector('#deadlist');
+ 
+  
+  var newDivNariga = new DivElement();
+  var newDivLefts = new DivElement();
+  var newDivTitles = new DivElement();
+  var newDivCoursename = new DivElement();
+  var newDivCoursetask = new DivElement();
+  
+  var newDivTools = new DivElement();
+  var newDivToolbutt1 = new DivElement();
+  var newDivToolbutt2 = new DivElement();
+  var newDivToolbutt3 = new DivElement();
+
+  // attacch classes
+  newDivNariga.classes.add('nariga');
+  newDivLefts.classes.add('lefts');
+  newDivTitles.classes.add('titles');
+  newDivCoursename.classes.add('coursename');
+  newDivCoursetask.classes.add('coursetask');
+  
+  newDivTools.classes.add('tools');
+  newDivToolbutt1.classes.add('toolbutt');
+  newDivToolbutt2.classes.add('toolbutt');
+  newDivToolbutt3.classes.add('toolbutt');
+  newDivToolbutt1.id = 'buttone' + id.toString();
+  newDivToolbutt2.id = 'butttwo' + id.toString();
+  newDivToolbutt3.id = 'buttthree' + id.toString();
+
+  // fill shit
+  newDivNariga.id = id.toString();
+  newDivCoursename.text = course;
+  newDivCoursetask.text = assign;
+  newDivLefts.text = deadline.toString(); // poi penso a fare i calcoli, mo printa u dat grezz
+  
+  newDivTools.append(newDivToolbutt1);
+  newDivTools.append(newDivToolbutt2);
+  newDivTools.append(newDivToolbutt3);
+
+  // putting pieces togheter
+  
+  
+  newDivTitles.append(newDivCoursename);
+  newDivTitles.append(newDivCoursetask);
+  newDivNariga.append(newDivLefts); // choose deads carefully then
+  
+  newDivNariga.append(newDivTools);
+  
+  newDivNariga.append(newDivTitles);
+  
 
   // add it
   deadList.children.add(newDivNariga);
 
   // fit it
-  setPlace(newDivLefts, deadline, id);
+  setPlace(newDivLefts, newDivTitles, deadline, id);
+  setButts(id);
+  
 
 }
 
+void setButts(id) {
+  
+  var buttone = '#buttone' + id.toString();
+  var butttwo = '#butttwo' + id.toString();
+  var buttthree = '#buttthree' + id.toString();
+  
+  var b1 = document.querySelector(buttone);
+  b1.setInnerHtml('''
+    <a href="javascript:void(0)" data-toggle="tooltip" data-placement="top"    title="Work it now!"   >
+    <span class="glyphicon glyphicon-fire" aria-hidden="true"></span>
+    </a>''', validator: _htmlValidator);
+  
+  int progress = 0;
+  
+  $(buttone).on('click', (QueryEvent e) {
+    
+    
+    var titles = ['Work it now!',
+                  '<b>You are working on this deadline...</b> <br> (Click here again AFTER you\'ll have submitted it!)',
+                  '<b>Completed!</b> <br> (Click again to RESET the status flag)'];
+    
+    var glyphs = ['glyphicon-fire',
+                  'glyphicon-fire red',
+                  'glyphicon-ok green'];
+    
+    progress++;
+    progress = progress > 2? 0 : progress;
+    print(progress);
+    
+    b1.setInnerHtml('''
+    <a href="javascript:void(0)" data-toggle="tooltip" data-placement="top"
+      title="${titles[progress]}"
+      data-html="true">
+    <span class="glyphicon ${glyphs[progress]}" aria-hidden="true"></span>
+    </a>''', validator: _htmlValidator);
+    
+   //Tooltip.wire($(buttone).children('[data-toggle]').firstIfAny); 
+    $('[data-toggle]').forEach((Element elem) => Tooltip.wire(elem));
+  });
+  
+  document.querySelector(butttwo).setInnerHtml('''
+    <a href="#" data-toggle="tooltip" data-placement="top"    title="Set alarm"   >
+    <span class="glyphicon glyphicon-bell" aria-hidden="true"></span>
+    </a>''', validator: _htmlValidator);
+  
+  document.querySelector(buttthree).setInnerHtml('''
+    <a href="#" data-toggle="tooltip" data-placement="top"    title="Remove deadline"   >
+    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+    </a>''', validator: _htmlValidator);
+  
+  $('[data-toggle]').forEach((Element elem) => Tooltip.wire(elem));
+}
 //
 // Look for nariga and adjust it!
 //
@@ -275,7 +458,8 @@ void adjustNariga(int id, int deadline) {
   for (var nariga in narigas) {
     if (int.parse(nariga.id) == id) {
       var n = nariga.querySelector('.lefts');
-      setPlace(n, deadline, id);
+      var t = nariga.querySelector('.titles');
+      setPlace(n, t, deadline, id);
       break;
     }
   }
@@ -313,10 +497,11 @@ String computeString(int leftsecs) {
     int hours = (leftsecs % SECS_1D) ~/ SECS_1H;
     int mins = (leftsecs % SECS_1H) ~/ SECS_1M;
 
-    (weeks > 0) ? sb.write(weeks.toString() + "w ") : null;
-    (days > 0) ? sb.write(days.toString() + "d ") : null;
-    (hours > 0) ? sb.write(hours.toString() + "h ") : null;
-    (mins > 0) ? sb.write(mins.toString() + "m") : null;
+    (weeks > 0) ? sb.write(weeks.toString() + "W ") : null;
+    (days > 0) ? sb.write(days.toString() + "D ") : null;
+    (hours > 0) ? sb.write(hours.toString() + "H ") : null;
+    (mins > 0) ? sb.write(mins.toString() + "M") : null;
+    (leftsecs < 60) ? sb.write(leftsecs.toString() + "S") : null;
 
   } else {
     sb.write("EXP"); // what to do then?
@@ -327,12 +512,27 @@ String computeString(int leftsecs) {
 }
 
 // Update css style to narigas
-void updateDead(dead, int leftsecs, int id) {
+void updateDead(dead, title, int leftsecs, int id) {
+  
+  int browserWidth = window.innerWidth;
+  int deadWidth = computeWidth(leftsecs, browserWidth);
+  int titleMaxWidth = browserWidth-deadWidth-100;
 
   dead.text = computeString(leftsecs);
   dead.style
-      ..width = '${computeWidth(leftsecs).toString()}%'
+      ..width = '${deadWidth.toString()}px'
       ..background = '#${computeBackground(leftsecs)}';
+  
+  if (leftsecs < 129600) {
+  var ct = title.querySelector('.coursetask');
+  ct.style
+      ..color = 'rgba(191, 47, 47, 0.87)';
+  }
+      
+// IMPORTATNE: RIABILITA STE DUE LINEE DI QUA SOTTO SE VUOI LA SIZE DEI TITOLI
+      //FIXATA ADATTIVA NELLA PAGINA
+  title.style
+      ..maxWidth = '${titleMaxWidth.toString()}px';
 
 }
 
@@ -341,12 +541,12 @@ bool alreadyPresent(int id) {
 }
 
 // IS this still useful?
-void setPlace(dead, int deadsecs, int id) {
+void setPlace(dead, title, int deadsecs, int id) {
   // hey, we have a new nariga, add to map:
   narigaMap.addAll({
     id: deadsecs
   }); // is this useful?
-  updateDead(dead, computeRemaining(deadsecs), id);
+  updateDead(dead, title, computeRemaining(deadsecs), id);
 }
 
 //
@@ -359,21 +559,39 @@ int computeRemaining(int deadsecs) {
   var leftsecs = diff.inSeconds;
 //  print(now.toLocal());
 //  print(then.toLocal()); // TO LOCAL!
-  (leftsecs > 0) ? print('left:' + leftsecs.toString()) : null;
+  //(leftsecs > 0) ? print('left:' + leftsecs.toString()) : null;
   return leftsecs;
 }
 
+String buildNarigaBody(int deadline, int id) {
+  var a = '<span class="label label-default">Due to: ' + getDueDate(deadline) + '</span>&nbsp;';
+  var b = '<a href=#><span class="label label-primary">Work it now!</span></a>&nbsp;';
+  var c = '''
+      Alarm: <div class="btn-group btn-toggle" data-toggle="buttons-checkbox">
+        <button class="btn btn-xs btn-primary active">6H</button>
+        <button class="btn btn-xs btn-default">12H</button>
+        <button class="btn btn-xs btn-default">1D</button>
+        <button class="btn btn-xs btn-default">2D</button>
+      </div>
+'''; // check it out here http://www.bootply.com/92189
+  // prob dovrai spostare sta roba e costrutrila poco per volta e settare l'onlckik event
+  return a+b+c;
+}
+String getDueDate(int deadline) {
+  DateTime then = new DateTime.fromMillisecondsSinceEpoch(deadline * 1000, isUtc: true);
+  return then.toLocal().toString();
+}
 
 //
 // Choose background according to left time
 // Current: http://www.colourlovers.com/palette/138026/a_beautiful_day
 //
 const BG_1H = '000';
-const BG_6H = 'FF003D';
-const BG_1D = 'FC930A';
-const BG_3D = 'F7C41F';
-const BG_1W = 'E0E05A';
-const BG_XN = 'CCF390';
+const BG_6H = 'F23C1F';//'FF4E50';//'FF003D';
+const BG_1D = 'F26F1D';//'FC913A';//'FC930A';
+const BG_3D = 'F2A41D';//'F9D423';//'F7C41F';
+const BG_1W = 'F2D91D';//'EDE574';//E0E05A';
+const BG_XN = 'E3E3E3';//'E1F5C4';//CCF390';
 
 String computeBackground(int leftsecs) {
 
@@ -395,10 +613,10 @@ String computeBackground(int leftsecs) {
 // How many secs to represent in one pixel?
 // (anyway I switch to percentages then)
 //
-const RAPPR_STEP_1H = 60; // <1h : 1px = 1min
-const RAPPR_STEP_6H = 300; // <6h : 1px = 5min
-const RAPPR_STEP_1D = 900; // <1d : 1px = 15min
-const RAPPR_STEP_3D = 1200; // <3d : 1px = 20min
+const RAPPR_STEP_1H = 60; // <1h : 1px = ?min orig:60 - 1min
+const RAPPR_STEP_6H = 300; // <6h : 1px = ?min orig:300
+const RAPPR_STEP_1D = 600; // <1d : 1px = 15min
+const RAPPR_STEP_3D = 900; // <3d : 1px = 20min
 const RAPPR_STEP_1W = 3600; // <1w : 1px = 25min
 const RAPPR_STEP_2W = 7200; // <1w : 1px = 25min
 const RAPPR_STEP_XN = 14400; // >1w : 1px = 30min
@@ -411,7 +629,7 @@ const PIX_1W = SECS_1W ~/ RAPPR_STEP_1W; // 465
 const PIX_2W = SECS_2W ~/ RAPPR_STEP_2W; // 465
 
 
-int computeWidth(int leftsecs) {
+int computeWidth(int leftsecs, int browserWidth) {
 
   var newwidth = 30; // default
 
@@ -505,8 +723,14 @@ int computeWidth(int leftsecs) {
   }
   //print((leftsecs - SECS_1H - SECS_6H - SECS_1D - SECS_3D - SECS_1W) ~/ RAPPR_STEP_XN);
 
-  newwidth = newwidth * 100 ~/ 800; // adapt to my choosen proportions
+  //newwidth = newwidth * 100 ~/ 800; // adapt to my choosen proportions
+  
+  newwidth = newwidth * browserWidth ~/ 1280 ; // I choose the proportions on 1280
+  
+  if (newwidth <= 150) {newwidth = 150;} // dont collapse any furthur.. but check this later joe
 
+  print(newwidth);
+  
   return newwidth;
 }
 
