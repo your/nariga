45cf5c9f2b5e360c04c88d59e7cc233789906d20
segfault <sf@sharped.net>
2015-08-22 20:02:44 +0200

    cleanup

--- old/icalendarupdater.rb
+++
@@ -1,9 +0,0 @@
-# Read JSON from file, iterate over objects
-file = File.read(default_destdir)
-
-parsed = JSON.parse(file)
-
-# all I need is urls
-parsed.each do |course|
-  p course["calurl"]
-end
